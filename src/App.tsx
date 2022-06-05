5e7bb8ebdd44f7d0755151031ac298f3526082f8
Christine <aqui.christine@gmail.com>
2022-06-05 00:11:44 -0400

    updated readme and fixes missed css to scss l-4

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-// import React from 'react';
 import Directory from './components/directory/directory.component';
 
-import './categories.styles.scss';
 
 const App = () => {
   const categories = [
